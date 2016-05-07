afa78830423842f0d6ed459f880d2fc5863092e0
Charles Cross <charles@openrov.com>
2016-05-06 17:44:29 -0700

    Updated to new stable 3.0.2 source tarball. Static only build

--- build.sh
+++ build.sh
@@ -2,15 +2,14 @@
 set -e
 
 # Get the FFMPEG source
-git clone git://source.ffmpeg.org/ffmpeg.git
+wget https://ffmpeg.org/releases/ffmpeg-3.0.2.tar.xz
 
-cd ffmpeg
-git checkout ce855bf43bcb3e217266653281db63c94e34c700
+tar xf ffmpeg-3.0.2.tar.xz
+cd ffmpeg*
 
 # Get library dependency sources
 git clone git://git.videolan.org/x264
 cd x264
-git checkout a01e33913655f983df7a4d64b0a4178abb1eb618
 
 # Build x264
 ./configure --enable-static --enable-pic --disable-asm --prefix=${PWD}/../../output/usr
@@ -19,7 +18,7 @@ make install
 cd ..
 
 # Build FFMPEG
-./configure --prefix=./build/ --enable-gpl --enable-libx264 --enable-static --enable-pic --extra-cflags="-I${PWD}/../output/usr/include/" --extra-ldflags="-L${PWD}/../output/usr/lib" --extra-libs="-ldl"
+./configure --prefix=./build/ --disable-vaapi --disable-shared --enable-gpl --enable-libx264 --enable-static --enable-pic --extra-cflags="-I${PWD}/../output/usr/include/" --extra-ldflags="-L${PWD}/../output/usr/lib" --extra-libs="-ldl"
 make -j8
 
 make prefix=${PWD}/../output/usr install
